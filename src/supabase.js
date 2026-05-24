8458bf2e53226c2c4ba01687bfcf2c8d8c164efc
pierregrabit16-cmyk <pierre.grabit16@gmail.com>
2026-05-24 09:52:02 +0200

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = 'https://lbhhzunpbehwaqpyctap.supabase.co'
-const SUPABASE_KEY = 'sb_publishable_nYKk9edn9fUblqwDdPK9eQ_C0u8dyLT'
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxiaGh6dW5wYmVod2FxcHljdGFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkzNjk3OTUsImV4cCI6MjA5NDk0NTc5NX0.m9K_rFsOCTcIng-GBS2XyVTsqPW4WG7brJu-ZERIK3Q'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
