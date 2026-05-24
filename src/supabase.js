fdf2756bb580d053fce525f34aec5b72620d7215
pierregrabit16-cmyk <pierre.grabit16@gmail.com>
2026-05-24 16:03:08 +0200

    Delete src directory

--- src/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const SUPABASE_URL = 'https://lbhhzunpbehwaqpyctap.supabase.co'
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxiaGh6dW5wYmVod2FxcHljdGFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkzNjk3OTUsImV4cCI6MjA5NDk0NTc5NX0.m9K_rFsOCTcIng-GBS2XyVTsqPW4WG7brJu-ZERIK3Q'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
